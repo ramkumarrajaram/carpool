8d2a5607117b0a715ece6a5fe841bb261d95e980
Ramkumar Rajaram <ramkumar_rajaram@IBMCLONEs-MacBook-Pro-2.local>
2020-04-20 21:58:33 +0800

    Add CarPool application for the project

---
+++ src/main/java/com/hackathon/CarPoolApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPoolApplication.class, args);
+    }
+}
