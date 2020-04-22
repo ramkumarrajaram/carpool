f76e569f36d955703348656913174358c6eacae7
Ramkumar Rajaram <ramkumar_rajaram@IBMCLONEs-MacBook-Pro-2.local>
2020-04-22 20:39:08 +0800

    Change repository to find by mobile number and add the details of create tables

--- src/main/java/com/sia/carpool/persistance/registeruser/RegisterUserRepository.java
+++ src/main/java/com/sia/carpool/persistance/registeruser/RegisterUserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RegisterUserRepository extends JpaRepository<RegisterUserEntity, String> {
 
+    RegisterUserEntity findByMobileNumber(String mobileNumber);
 }
