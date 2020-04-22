94f993cf8d6b92cde9bc35ce7c4b7022ac3cd624
Ramkumar Rajaram <ramkumar_rajaram@IBMCLONEs-MacBook-Pro-2.local>
2020-04-22 20:40:52 +0800

    Add service and controller for Publish riders

---
+++ src/main/java/com/sia/carpool/publishride/PublishRideService.java
@@ -0,0 +1,38 @@
+package com.sia.carpool.publishride;
+
+import com.sia.carpool.CarPoolException;
+import com.sia.carpool.persistance.publishride.PublishRideEntity;
+import com.sia.carpool.persistance.publishride.PublishRideRepository;
+import com.sia.carpool.persistance.registeruser.RegisterUserEntity;
+import com.sia.carpool.persistance.registeruser.RegisterUserRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PublishRideService {
+
+
+    private ModelMapper modelMapper;
+    private RegisterUserRepository registerUserRepository;
+    private PublishRideRepository publishRideRepository;
+
+    public void publishRide(PublishRideInput rideInput) {
+        PublishRideEntity publishRideEntity = modelMapper
+                .map(rideInput, PublishRideEntity.class);
+
+        RegisterUserEntity userEntity = registerUserRepository.findByMobileNumber(rideInput.getMobileNumber());
+
+        if (null == userEntity) {
+            throw new CarPoolException("User not registered");
+        }
+        publishRideEntity.setUserName(userEntity.getUserName());
+
+        try {
+            publishRideRepository.save(publishRideEntity);
+        } catch (Exception e) {
+            throw new CarPoolException("Error while saving the driver details " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/sia/carpool/publishride/web/PublishRideController.java
@@ -0,0 +1,42 @@
+package com.sia.carpool.publishride.web;
+
+import com.sia.carpool.publishride.PublishRideInput;
+import com.sia.carpool.publishride.PublishRideService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.CacheControl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.http.HttpHeaders.CACHE_CONTROL;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/ride")
+public class PublishRideController {
+
+    private ModelMapper modelMapper;
+    private PublishRideService publishRideService;
+
+    @PostMapping("/publish")
+    public void publishRide(HttpServletRequest request,
+                            @RequestBody PublishRideRequest publishRideRequest,
+                            HttpServletResponse response) {
+        PublishRideInput rideInput = modelMapper
+                .map(publishRideRequest, PublishRideInput.class);
+
+        publishRideService.publishRide(rideInput);
+
+        response.setHeader(
+                CACHE_CONTROL,
+                CacheControl.noStore().getHeaderValue()
+        );
+
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+}
